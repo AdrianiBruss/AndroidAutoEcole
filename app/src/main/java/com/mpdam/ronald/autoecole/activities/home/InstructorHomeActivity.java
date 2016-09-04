b4adce18af93c382b1697dfafbc4d554146c4295
RPDevFront <ronald.developpeur@gmail.com>
2016-09-04 20:48:54 +0200

    instructor home integration

--- app/src/main/java/com/mpdam/ronald/autoecole/activities/home/InstructorHomeActivity.java
+++ app/src/main/java/com/mpdam/ronald/autoecole/activities/home/InstructorHomeActivity.java
@@ -4,11 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mpdam.ronald.autoecole.R;
 import com.mpdam.ronald.autoecole.activities.account.LoginActivity;
 import com.mpdam.ronald.autoecole.activities.capture.CaptureActivity;
+import com.mpdam.ronald.autoecole.adapters.StudentAdapter;
 import com.mpdam.ronald.autoecole.models.Instructor;
 import com.mpdam.ronald.autoecole.models.Student;
 import com.mpdam.ronald.autoecole.modelsRepositories.InstructorRepository;
@@ -18,11 +25,22 @@ import com.strongloop.android.loopback.RestAdapter;
 import com.strongloop.android.loopback.callbacks.ObjectCallback;
 import com.strongloop.android.loopback.callbacks.VoidCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstructorHomeActivity extends AppCompatActivity {
 
     private InstructorRepository instructorRepo;
 
     private RestAdapter adapter;
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +50,15 @@ public class InstructorHomeActivity extends AppCompatActivity {
         adapter = new RestAdapter(getApplicationContext(), Constant.URL);
         instructorRepo = adapter.createRepository(InstructorRepository.class);
 
+//        mListView = (ListView) findViewById(R.id.listViewStudents);
 
+        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
+        //Contenant une TextView avec comme identifiant "@android:id/text1"
+
+//        List<Student> students = genererStudents();
+//
+//        StudentAdapter adapter = new StudentAdapter(InstructorHomeActivity.this, students);
+//        mListView.setAdapter(adapter);
 //        Log.e("current user", Constant.USER.toString());
     }
 
@@ -49,6 +75,17 @@ public class InstructorHomeActivity extends AppCompatActivity {
                 Log.e("on error", "logout");
             }
         });
+    }
+
+    private List<Student> genererStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student("", "153A rue Cardinet", "Florent", "Philippon", "0102030405", ""));
+        students.add(new Student("", "19 avenue Jean Jaurès", "Jean", "Dupont", "0899451369", ""));
+        students.add(new Student("", "175 rue Cardinet", "Jacques", "Dutreix", "0714236541", ""));
+        students.add(new Student("", "15 rue Clichy", "Luc", "Michelin", "0555041082", ""));
+        students.add(new Student("", "39 bis boulevard des Batignolles", "Thomas", "Hammerschmidt", "0987652412", ""));
+        students.add(new Student("", "153A rue des Clochards", "Julie", "Renaud", "0659724047", ""));
+        return students;
     }
 
 //    public void capturePicture(View view) {
